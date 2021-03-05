7cff8b5bd4c0004285a6287017db165c0e4e4d7f
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-03-05 20:56:51 +0100

    feat: parseo los argumentos de entrada

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::env;
+use std::error::Error;
+use std::boxed::Box;
+use std::process::exit;
+use simple_error::bail;
+
+/// Representa los parametros del programa
+/// Estos son los que ha introducido el dato por la linea de comandos
+#[derive(Debug)]
+struct ProgramParameters{
+    data_file: String,
+    constraints_file: String,
+    seed: i32
+}
+
+fn parse_args(args: Vec<String>) -> Result<ProgramParameters, Box<dyn Error>>{
+    if args.len() != 4{
+        bail!("3 parameters expected, {} given", args.len() - 1)
+    }
+
+    let data_file = args[1].parse::<String>()?;
+    let constraints_file = args[2].parse::<String>()?;
+    let seed = args[3].parse::<i32>()?;
+
+    return Ok(ProgramParameters{
+        data_file, constraints_file, seed
+    });
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program_arguments = match parse_args(args){
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("No se pudo leer los parametros dados por terminal");
+            eprintln!("[Errcode]: {}", err);
+            exit(-1);
+        }
+    };
+
+    println!("{:?}", program_arguments);
 }
