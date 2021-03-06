94a08deb8e63df7299a331304915335275df41e5
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-03-06 17:46:13 +0100

    feat: parseo el fichero de restricciones y soluciono un bug
    El bug era que al estar usando un csvreader ignoraba la primera linea al
    pensar que eran las cabeceras

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,13 @@ struct ProgramParameters{
     seed: i32
 }
 
+/// Representa el conjunto de puntos que hay que agrupar
 #[derive(Debug)]
 struct DataPoints{
     points: Vec<Point>
 }
 
+/// Representa un punto
 #[derive(Debug)]
 struct Point{
     coordinates: Vec<f32>
@@ -46,7 +48,9 @@ fn parse_data_file_to_struct(data_path: &str) -> Result<DataPoints, Box<dyn Erro
     // Tiene que ser mutable para poder iterar
     // El proceso de tomar el siguiente elemento se considera una mutacion de
     // la variable
-    let mut reader = csv::Reader::from_path(data_path)?;
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false) // Nuestro fichero no tiene headers
+        .from_path(data_path)?;
     let mut points: Vec<Point> = vec![];
 
     for current_line in reader.records(){
@@ -70,6 +74,65 @@ fn parse_data_file_to_struct(data_path: &str) -> Result<DataPoints, Box<dyn Erro
     return Ok(DataPoints{points});
 }
 
+#[derive(Debug)]
+enum ConstraintType{
+    MustLink,
+    CannotLink,
+}
+
+/// Estructura de datos que representa una restriccion
+/// Una restriccion viene dada por los dos indices de los elementos que se
+/// restringen y el tipo de restriccion
+// TODO -- pasar esta estructura de datos a un hash para tener acceso directo
+#[derive(Debug)]
+struct Constraint{
+    first_index: i32,
+    second_index: i32,
+    constraint_type: ConstraintType
+}
+
+fn parse_constraints_file_to_struct(constraint_file_path: &str) -> Result<Vec<Constraint>, Box<dyn Error>>{
+    let mut constraints: Vec<Constraint> = vec![];
+
+
+    // Tiene que ser mutable para poder iterar
+    // El proceso de tomar el siguiente elemento se considera una mutacion de
+    // la variable
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false) // Nuestro fichero no tiene headers
+        .from_path(constraint_file_path)?;
+
+
+    for (index, current_line) in reader.records().enumerate(){
+        // Unwrap el result
+        let current_line = current_line?;
+
+        // Mapear StringRecord a un vector de enteros
+        let data: Vec<&str> = current_line.iter().collect();
+        let data: Vec<String> = data.into_iter().map(|x| x.to_string()).collect();
+
+        // Si algun elemento no se puede parsear, se devuelve un error
+        // En otro caso, me tomo los datos
+        let data: Result<Vec<i32>, _> = data.into_iter().map(|x| x.parse::<i32>()).collect();
+        let data = data?;
+
+        // Recorremos el vetor de restricciones y añadimos las restricciones que nos encontremos
+        for (second_index, value) in data.into_iter().enumerate(){
+            if value == 1{
+                let constraint = Constraint{first_index: index as i32, second_index: second_index as i32, constraint_type: ConstraintType::MustLink};
+                constraints.push(constraint);
+            }
+
+            if value == -1{
+                let constraint = Constraint{first_index: index as i32, second_index: second_index as i32, constraint_type: ConstraintType::CannotLink};
+                constraints.push(constraint);
+            }
+        }
+    }
+
+    return Ok(constraints);
+
+}
 fn show_help(){
     println!("Modo de uso del programa:");
     println!("\t./PracticasMetaheuristicas <data_file> <constraints_file> <seed>")
@@ -99,5 +162,13 @@ fn main() {
         }
     };
 
-    println!("Data points: {:?}", data_points);
+    // Parseamos los datos del archivo de restricciones
+    let constraints = match parse_constraints_file_to_struct(&program_arguments.constraints_file){
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("No se pudieron leer los datos de restricciones del fichero {}", program_arguments.constraints_file);
+            eprintln!("[Errcode]: {}", err);
+            exit(-1);
+        }
+    };
 }
