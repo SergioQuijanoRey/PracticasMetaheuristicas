ee26b9e71616a71e568e64dfb97eed023088a1b4
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-03-07 19:46:40 +0100

    feat: introduzo a mano restricciones en la caida en locales de la busqueda loca. TODO -- Esto hay que cambiarlo

--- src/busqueda_local.rs
+++ src/busqueda_local.rs
@@ -9,6 +9,8 @@ pub fn run(data_points: DataPoints, constraints: Vec<Constraint>, number_of_clus
     let mut current_solution = Solution::generate_random_solution(data_points, constraints, number_of_clusters);
     println!("Solucion inicial aleatoria: {:?}", current_solution.get_cluster_indexes());
 
+    let mut iterations_since_improvement = 0;
+
     // Realizamos las iteraciones pertinentes
     for i in 0..max_iterations{
 
@@ -23,11 +25,22 @@ pub fn run(data_points: DataPoints, constraints: Vec<Constraint>, number_of_clus
         if new_solution.fitness() < current_solution.fitness(){
             println!("Mejoramos el fitness de {} a {}", current_solution.fitness(), new_solution.fitness());
             current_solution = new_solution;
+
+            // TODO -- BUG -- esto hay que gestionarlo en el struct solucion
+            iterations_since_improvement = 0;
+        }else{
+            iterations_since_improvement += 1;
         }
 
         // No podemos mejorar mas el fitness
         if current_solution.fitness() == 0.0{
             println!("Solucion optima encontrada, nos ahorramos {} iteraciones", max_iterations - i);
+            break;
+        }
+
+        if iterations_since_improvement == 1000{
+            println!("TODO -- 1000 iteraciones sin mejorar la solucion");
+            println!("Paramos porque nos hemos atascado supuestamente en un optimo local");
             break;
         }
     }
