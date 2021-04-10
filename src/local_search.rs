c4c52bb4d38ce49bf246171b43aebfbabb309525
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-04-10 19:38:59 +0200

    feat: ahora tambien guardamos la evolucion del fitness en local_search

--- src/local_search.rs
+++ src/local_search.rs
@@ -1,13 +1,17 @@
 use crate::problem_datatypes::Solution;
 use crate::problem_datatypes::DataPoints;
 use crate::problem_datatypes::Constraints;
+use crate::problem_datatypes::FitnessEvolution;
 use rand::rngs::StdRng;
 
 /// Ejecuta la metaheuristica de busqueda local y devuelve la solucion encontrada
-pub fn run<'a, 'b>(data_points: &'a DataPoints, constraints: &'b Constraints, number_of_clusters: i32, max_iterations: i32, rng: &mut StdRng) -> Solution<'a, 'b>{
+pub fn run<'a, 'b>(data_points: &'a DataPoints, constraints: &'b Constraints, number_of_clusters: i32, max_iterations: i32, rng: &mut StdRng) -> (Solution<'a, 'b>, FitnessEvolution){
+    // Cuenta de como avanza la evolucion del fitness a traves de las iteraciones
+    let mut fitness_evolution = FitnessEvolution::new();
 
     // Partimos de una solucion inicial aleatoria
     let mut current_solution = Solution::generate_random_solution(data_points, constraints, number_of_clusters, rng);
+    fitness_evolution.add_iteration(current_solution.fitness());
 
     // Realizamos las iteraciones pertinentes
     for i in 0..max_iterations{
@@ -19,15 +23,15 @@ pub fn run<'a, 'b>(data_points: &'a DataPoints, constraints: &'b Constraints, nu
             // No hemos encontrado ningun vecino mejor, asi que paramos de iterar
             // Ademas mostramos la informacion de las iteraciones que nos hemos ahorrado
             None => {
-                println!("Nos ahorramos {} iteraciones", max_iterations - i);
                 break;
             },
         };
 
-        // Hacemos el cambio de solucion ahora que ya hemos mostrado la mejora de fitness
+        // Hacemos el cambio de solucion y guardamos la mejora del fitness
         current_solution = new_solution;
+        fitness_evolution.add_iteration(current_solution.fitness());
 
     }
 
-    return current_solution;
+    return (current_solution, fitness_evolution);
 }
--- src/main.rs
+++ src/main.rs
@@ -170,7 +170,7 @@ fn main() {
             let max_iterations = 100000;
 
             let before = Instant::now();
-            let solucion_local = local_search::run(&data_points, &constraints, program_arguments.get_number_of_clusters(), max_iterations, &mut rng);
+            let (solucion_local, fitness_evolution) = local_search::run(&data_points, &constraints, program_arguments.get_number_of_clusters(), max_iterations, &mut rng);
             let after = Instant::now();
             let duration = after.duration_since(before);
             let duration_numeric = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
@@ -182,6 +182,7 @@ fn main() {
             println!("El valor de fitness es: {}", solucion_local.fitness());
             println!("El valor de lambda es: {}", solucion_local.get_lambda());
             println!("Tiempo transcurrido (segundos): {}", duration_numeric);
+            println!("Evolucion del fitness: {}", fitness_evolution);
             println!("");
         }
     }
