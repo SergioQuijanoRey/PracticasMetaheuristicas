93c8d4a48ba5642b53396f8c35cf73708ab55bda
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-05-21 14:07:10 +0200

    refactor: evitamos usar codigo repetido en la local search

--- src/algorithms/local_search.rs
+++ src/algorithms/local_search.rs
@@ -34,56 +34,18 @@ pub fn run_and_show_results(data_points: &DataPoints, constraints: &Constraints,
 }
 
 /// Ejecuta la metaheuristica de busqueda local y devuelve la solucion encontrada
-/// Funcion publica porque se usa en la busqueda multiarranque basica
-// TODO -- usar el metodo run_from_init_sol para no copiar demasiado codigo
+/// Parte de una solucion inicial aleatoria
 pub fn run<'a, 'b>(data_points: &'a DataPoints, constraints: &'b Constraints, number_of_clusters: i32, max_fitness_evaluations: i32, rng: &mut StdRng) -> (Solution<'a, 'b>, FitnessEvolution){
-    // Cuenta de como avanza la evolucion del fitness a traves de las iteraciones
-    let mut fitness_evolution = FitnessEvolution::new();
-
     // Partimos de una solucion inicial aleatoria
-    let mut current_solution = Solution::generate_random_solution(data_points, constraints, number_of_clusters, rng);
-    fitness_evolution.add_iteration(current_solution.fitness());
+    let init_sol = Solution::generate_random_solution(data_points, constraints, number_of_clusters, rng);
 
-    // Realizamos las iteraciones pertinentes mientras no hayamos consumido todas las evaluaciones
-    // sobre el fitness
-    let mut fitness_evaluations_consumed = 0;
-    while fitness_evaluations_consumed < max_fitness_evaluations{
-
-        // Las evaluaciones de fitness que se consumen en esta iteracion
-        let mut current_fitness_consumed = 0;
-
-        // Tomamos el vecino y tenemos en cuenta las evaluaciones del fitness consumidas
-        let evaluations_left = max_fitness_evaluations - fitness_evaluations_consumed;
-        let find_new_solution_result = current_solution.get_neighbour(evaluations_left, rng);
-        let new_solution = find_new_solution_result.get_result();
-        current_fitness_consumed += find_new_solution_result.get_iterations_consumed();
-
-        let new_solution = match new_solution{
-            Some(sol) => sol,
-
-            // No hemos encontrado ningun vecino mejor, asi que paramos de iterar
-            // Ademas mostramos la informacion de las iteraciones que nos hemos ahorrado
-            None => {
-                break;
-            },
-        };
-
-        // Hacemos el cambio de solucion y guardamos la mejora del fitness
-        // Este valor del fitness ya ha sido calculado en la busqueda del vecinadario, y por tanto,
-        // no consume evaluaciones del fitness
-        current_solution = new_solution.clone();
-        fitness_evolution.add_iteration(current_solution.fitness());
-        debug_assert!(current_solution.is_fitness_cached() == true, "El vecino generado debe tener el valor del fitness cacheado");
-
-        // Añadimos todas las evaluaciones que se hayan consumido en la busqueda
-        fitness_evaluations_consumed += current_fitness_consumed as i32;
-    }
-
-    return (current_solution, fitness_evolution);
+    // Usamos la funcion que corre la busqueda local cuando se le da la solucion inicial
+    return run_from_init_sol(max_fitness_evaluations, &init_sol, rng);
 }
 
-/// Lanza la busqueda local, pero en vez de con una solucion inicial aleatoria, se da el valor de
-/// la solucion inicial
+/// Lanza la busqueda local.
+/// Se necesita como argumento una solucion inicial.
+/// Usar run si se quiere solucion inicial aleatoria
 pub fn run_from_init_sol<'a, 'b>(max_fitness_evaluations: i32, init_sol: &Solution<'a, 'b>, rng: &mut StdRng) -> (Solution<'a, 'b>, FitnessEvolution){
     // Cuenta de como avanza la evolucion del fitness a traves de las iteraciones
     let mut fitness_evolution = FitnessEvolution::new();
